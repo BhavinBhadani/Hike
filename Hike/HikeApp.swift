bc9eb775494aca782a240d0c1c4e3cb0f133d1ce
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-14 09:23:48 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Bhavin Bhadani on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
